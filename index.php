f62a40583c51af5544e15864e965c646767b8192
DenyLibrayardy <denylibra04@gmail.com>
2022-01-13 15:31:48 +0700

    menambahkan angka

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, accusantium? Dolorem omnis quibusdam iste dicta animi velit odio ad quas inventore, ea impedit sapiente id accusamus quam. Et, sint temporibus.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
